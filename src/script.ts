426a420bdee7ac83616b75d1cc62a66e0bea5423
Vaagn Avanesyan <vahagn1993@gmail.com>
2023-08-17 12:42:59 +0300

    feat(*): finish lesson 5

--- src/script.ts
+++ src/script.ts
@@ -1,22 +1,56 @@
-import {BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from "three";
+import {
+  AxesHelper,
+  BoxGeometry, Group,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer
+} from "three";
 
-const canvas = document.querySelector('canvas.webgl')
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600
+};
 
-const geometry = new BoxGeometry(1, 1, 1)
-const material = new MeshBasicMaterial({color: 0xff0000})
-const mesh = new Mesh(geometry, material)
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new Mesh(geometry, material);
+mesh.position.set(1.7, 1.6, 0.6);
+mesh.scale.set(2, 0.25, 0.5);
 
-const camera = new PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+const camera = new PerspectiveCamera(100, sizes.width / sizes.height);
+camera.position.set(1, 1, 3);
+// camera.lookAt(mesh.position);
+const axesHelper = new AxesHelper();
+
+const group = new Group();
+const cube1 = new Mesh(
+  new BoxGeometry(1, 1, 1),
+  new MeshBasicMaterial({ color: "orange" })
+);
+const cube2 = new Mesh(
+  new BoxGeometry(1, 1.5, 1),
+  new MeshBasicMaterial({ color: "blue" })
+);
+cube2.position.x = -2;
+const cube3 = new Mesh(
+  new BoxGeometry(1, 1.5, 1),
+  new MeshBasicMaterial({ color: "lime" })
+);
+cube3.position.x = 2;
+group.add(cube1);
+group.add(cube2);
+group.add(cube3)
+group.rotation.x = 1
 
 const scene = new Scene();
-scene.add(mesh)
-scene.add(camera)
+scene.add(mesh);
+scene.add(group);
+scene.add(axesHelper);
+scene.add(camera);
 
-const renderer = new WebGLRenderer({canvas})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+const canvas = document.querySelector("canvas.webgl");
+const renderer = new WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
