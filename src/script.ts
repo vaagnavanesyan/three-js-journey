812b37d4a8405c0d972546dad19f8af5b6b2f63b
Vaagn Avanesyan <vahagn1993@gmail.com>
2023-08-17 22:47:15 +0300

    feat(lesson-6): animate by gsap

--- src/script.ts
+++ src/script.ts
@@ -1,20 +1,13 @@
-import {
-  AxesHelper,
-    BoxGeometry, Clock, Group,
-  Mesh,
-  MeshBasicMaterial,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer
-} from "three";
+import {BoxGeometry, Group, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from "three";
+import gsap from 'gsap'
 
 const sizes = {
-  width: 800,
-  height: 600
+    width: 800,
+    height: 600
 };
 
 const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({ color: 0xff0000 });
+const material = new MeshBasicMaterial({color: 0xff0000});
 const mesh = new Mesh(geometry, material);
 mesh.position.set(3.7, 1.6, 0.6);
 mesh.scale.set(2, 0.25, 0.5);
@@ -24,17 +17,17 @@ camera.position.set(1, 1, 3);
 
 const group = new Group();
 const cube1 = new Mesh(
-  new BoxGeometry(1, 1, 1),
-  new MeshBasicMaterial({ color: "orange" })
+    new BoxGeometry(1, 1, 1),
+    new MeshBasicMaterial({color: "orange"})
 );
 const cube2 = new Mesh(
-  new BoxGeometry(1, 1.5, 1),
-  new MeshBasicMaterial({ color: "blue" })
+    new BoxGeometry(1, 1.5, 1),
+    new MeshBasicMaterial({color: "blue"})
 );
 cube2.position.x = -2;
 const cube3 = new Mesh(
-  new BoxGeometry(1, 1.5, 1),
-  new MeshBasicMaterial({ color: "lime" })
+    new BoxGeometry(1, 1.5, 1),
+    new MeshBasicMaterial({color: "lime"})
 );
 cube3.position.x = 2;
 group.add(cube1);
@@ -47,14 +40,12 @@ scene.add(group);
 scene.add(camera);
 
 const canvas = document.querySelector("canvas.webgl");
-const renderer = new WebGLRenderer({ canvas });
+const renderer = new WebGLRenderer({canvas});
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
-const clock = new Clock();
+
+gsap.to(mesh.position, {x: 2, duration: 1, delay: 1.2})
 const tick = () => {
-    camera.position.x = Math.sin(clock.getElapsedTime());
-    camera.position.z = Math.cos(clock.getElapsedTime());
-    camera.lookAt(mesh.position)
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick)
 }
