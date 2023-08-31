038d32c6e712f90984338fc6d765e3def7d35029
Vaagn Avanesyan <vahagn1993@gmail.com>
2023-09-01 00:01:31 +0300

    feat(*): prepare to lesson 12

--- src/script.ts
+++ src/script.ts
@@ -1,53 +1,79 @@
-import {BoxGeometry, Group, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from "three";
-import gsap from 'gsap'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+THREE.ColorManagement.enabled = false
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector<HTMLCanvasElement>('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Sizes
+ */
 const sizes = {
-    width: 800,
-    height: 600
-};
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
-const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({color: 0xff0000});
-const mesh = new Mesh(geometry, material);
-mesh.position.set(3.7, 1.6, 0.6);
-mesh.scale.set(2, 0.25, 0.5);
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-const camera = new PerspectiveCamera(100, sizes.width / sizes.height);
-camera.position.set(1, 1, 3);
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-const group = new Group();
-const cube1 = new Mesh(
-    new BoxGeometry(1, 1, 1),
-    new MeshBasicMaterial({color: "orange"})
-);
-const cube2 = new Mesh(
-    new BoxGeometry(1, 1.5, 1),
-    new MeshBasicMaterial({color: "blue"})
-);
-cube2.position.x = -2;
-const cube3 = new Mesh(
-    new BoxGeometry(1, 1.5, 1),
-    new MeshBasicMaterial({color: "lime"})
-);
-cube3.position.x = 2;
-group.add(cube1);
-group.add(cube2);
-group.add(cube3)
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
-const scene = new Scene();
-scene.add(mesh);
-scene.add(group);
-scene.add(camera);
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
 
-const canvas = document.querySelector("canvas.webgl");
-const renderer = new WebGLRenderer({canvas});
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
-gsap.to(mesh.position, {x: 2, duration: 1, delay: 1.2})
-const tick = () => {
-    renderer.render(scene, camera);
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
-tick();
+tick()
